482dc6db78c11b68a7932b2b456cb0ff0519765e
Witria13 <witriadsra@gmail.com>
2020-10-02 11:50:16 +0700

    New Thread Form

--- app/controllers/forum_threads_controller.rb
+++ app/controllers/forum_threads_controller.rb
@@ -6,4 +6,8 @@ class ForumThreadsController < ApplicationController
 	def show
 		@thread = ForumThread.find(params[:id])
 	end
+
+	def new
+		@thread =  ForumThread.new
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "forum_threads#index"
-  resources :forum_threads, only: [:show]
+  resources :forum_threads, only: [:show, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
