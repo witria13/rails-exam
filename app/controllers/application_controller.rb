79a2b93a377ec176da765b17d72e170cbebd14a7
Witria13 <witriadsra@gmail.com>
2020-10-06 15:50:12 +0700

    hanya admin yang bisa edit dan hapus

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # debugger
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def registration_paramss
+    debugger
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  end
 end
